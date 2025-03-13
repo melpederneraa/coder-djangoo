569b00351b30492dc14029cb4ea14b259acc204e
melpederneraa <melipedernera2004@gmail.com>
2025-03-13 17:59:08 -0300

    feat:Respuesta simple

--- core/views.py
+++ core/views.py
@@ -1,3 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def saludar (request):
+    return HttpResponse('Hola desde Django')
+def saludar_con_etiqueta(request):
+    return HttpResponse('<h1> Hola con etiquetas</h1>')
